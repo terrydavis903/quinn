ad9e3801264de441e24be2a4cd64353dee3ae3d0
0xDEADBEAT <jasperbriggs677@gmail.com>
2024-08-12 16:48:19 -0400

    maybe more stable

--- quinn/src/runtime/tokio.rs
+++ quinn/src/runtime/tokio.rs
@@ -60,7 +60,7 @@ impl AsyncUdpSocket for UdpSocket {
         let io = &self.io;
         loop {
             
-            ready!(io.poll_send_ready(cx))?;
+            // ready!(io.poll_send_ready(cx))?;
             if let Ok(res) = io.try_io(Interest::WRITABLE, || {
                 debug!("poll sending packets: {}", transmits.len());
                 inner.send_proxy(io.into(), state, transmits)
@@ -77,7 +77,7 @@ impl AsyncUdpSocket for UdpSocket {
         meta: &mut [udp::RecvMeta],
     ) -> Poll<io::Result<usize>> {
         loop {
-            ready!(self.io.poll_recv_ready(cx))?;
+            // ready!(self.io.poll_recv_ready(cx))?;
             
             let io_res = self.io.try_io(Interest::READABLE, || {
                 self.inner.recv_proxy((&self.io).into(), bufs, meta)
